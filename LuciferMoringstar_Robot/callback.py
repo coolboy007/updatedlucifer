3b82f7a1a169eef270418dccb0056902a2f2f2e5
coolboy007 <61577388+coolboy007@users.noreply.github.com>
2022-03-17 20:21:15 +0530

    Update callback.py

--- LuciferMoringstar_Robot/callback.py
+++ LuciferMoringstar_Robot/callback.py
@@ -236,7 +236,7 @@ async def cb_handler(client: LuciferMoringstar_Robot, query):
                 caption=CUSTOM_FILE_CAPTION.format(mention=query.from_user.mention, title=title, size=size, caption=files.caption)
 
                 buttons = [[
-                  InlineKeyboardButton('🧑‍💻 How To Own 🧑‍💻', url='https://t.me/Mo_Tech_Group')
+                  InlineKeyboardButton('🧑‍💻 JOIN BACKUP 🧑‍💻', url='https://t.me/sdmoviesflixofficial')
                   ]]                 
                 
                 await query.answer()
@@ -245,6 +245,7 @@ async def cb_handler(client: LuciferMoringstar_Robot, query):
                     file_id=file_id,
                     caption=caption,
                     reply_markup=InlineKeyboardMarkup(buttons)
+                     protect_content=True if ident == "filep" else False,
                     )
 
 
@@ -259,8 +260,8 @@ async def cb_handler(client: LuciferMoringstar_Robot, query):
                  InlineKeyboardButton("ℹ️ Help", callback_data="help"),
                  InlineKeyboardButton("😎 About", callback_data="about") 
                  ],[
-                 InlineKeyboardButton("🗳 Deploy", url="https://youtu.be/FCU_XJmyG_U"),
-                 InlineKeyboardButton("🤖 Update", url="https://t.me/Mo_Tech_Group")
+                 InlineKeyboardButton("🗳 Backup", url="https://t.me/sdmoviesflixback"),
+                 InlineKeyboardButton("🤖 Update", url="https://t.me/sdmoviesflixofficial")
                  ]]
             else:
                 buttons = [[
@@ -269,8 +270,8 @@ async def cb_handler(client: LuciferMoringstar_Robot, query):
                  InlineKeyboardButton("ℹ️ Help", callback_data="bot_owner"),
                  InlineKeyboardButton("😎 About", callback_data="about") 
                  ],[
-                 InlineKeyboardButton("🗳 Deploy", url="https://youtu.be/FCU_XJmyG_U"),
-                 InlineKeyboardButton("🤖 Update", url="https://t.me/Mo_Tech_Group")
+                 InlineKeyboardButton("🗳 Backup", url="https://t.me/sdmoviesflixback"),
+                 InlineKeyboardButton("🤖 Update", url="https://t.me/sdmoviesflixofficial")
                  ]]               
             await query.message.edit(text=START_MSG.format(mention=query.from_user.mention, bot_name=bot_info.BOT_NAME, bot_username=bot_info.BOT_USERNAME), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
