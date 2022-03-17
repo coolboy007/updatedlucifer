9884bfa2638f642441bb968fd9f45faa15eb2559
coolboy007 <61577388+coolboy007@users.noreply.github.com>
2022-03-17 19:20:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/PR0FESS0R-99/LuciferMoringstar-Robot.git /LuciferMoringstar-Robot
+  git clone https://github.com/coolboy007/updatedlucifer.git /updatedlucife
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LuciferMoringstar-Robot
+  git clone $UPSTREAM_REPO /updatedlucife
 fi
 cd /LuciferMoringstar-Robot
 pip3 install -U -r requirements.txt
