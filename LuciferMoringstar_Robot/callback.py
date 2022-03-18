e6e3f6233fb5ab98eea240045d177c1bcf5a314a
coolboy007 <61577388+coolboy007@users.noreply.github.com>
2022-03-18 20:13:09 +0530

    Update callback.py

--- LuciferMoringstar_Robot/callback.py
+++ LuciferMoringstar_Robot/callback.py
@@ -247,7 +247,7 @@ async def cb_handler(client: LuciferMoringstar_Robot, query):
                     file_id=file_id,
                     caption=caption,
                     reply_markup=InlineKeyboardMarkup(buttons)
-                  protect_content=True if pre == 'filep' else False,
+                   protect_content=msg.get('protect', False),
                     )
 
 
